12305a27a6ecfba7ee65c89a7e9e692d4776833a
Aritra <theroyaritra@gmail.com>
2022-08-16 06:04:31 +0530

    Solving Deployment errors 3

--- market/__init__.py
+++ market/__init__.py
@@ -5,7 +5,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://uufzylpvhjkirs:c415913666e736e75dd1203b53533ad89897b7179968035b156691bee771b64b@ec2-44-195-100-240.compute-1.amazonaws.com:5432/d62mapd7sqku08"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '9203193009476448d996ce6d'
 db = SQLAlchemy(app)
